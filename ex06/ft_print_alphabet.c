d2b3d8795f8f74bc0872d38b578fc636c246dc12
emmmilla <emmmilla@student.42madrid.com>
2026-05-23 17:52:45 +0200

    First Upload ex06

---
+++ ex06/ft_print_alphabet.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_alphabet.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: emmmilla <emmmilla@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/22 10:24:45 by emmmilla          #+#    #+#             */
+/*   Updated: 2026/05/23 17:34:25 by emmmilla         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c);
+/*{
+	write(1, &c, 1);
+}*/
+
+// Function that  displays the lowercase alphabet on a single line,
+// in ascending order, starting with the letter “a”
+void	ft_print_alphabet(void)
+{
+	char	c;
+
+	c = 'a';
+	while (c >= 'a' && c <= 'z')
+	{
+		ft_putchar(c);
+		c++;
+	}
+}
+
+/*//Testing the function
+int	main(void)
+{
+	ft_print_alphabet();
+	return (0);
+}*/
