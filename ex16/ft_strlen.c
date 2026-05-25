41d8beabddba2839172822c42b98666d6f4e7076
emmmilla <emmmilla@student.42madrid.com>
2026-05-25 10:20:08 +0200

    First Upload ex16

---
+++ ex16/ft_strlen.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sqrt.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: emmmilla <emmmilla@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/22 13:16:00 by emmmilla          #+#    #+#             */
+/*   Updated: 2025/11/20 19:11:13 by emmmilla         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_strlen(char *str)
+{
+	int	len;
+
+	len = 0;
+	while (str[len] != '\n')
+	{
+		len++;
+	}
+	return (len);
+}
+
+/*//Testing the function with some values
+#include <stdio.h>
+
+int	main(void)
+{
+	char	*test = "Emmanuel Millan";
+	int		len;
+
+	len = ft_strlen(test);
+	printf("The string lengh of '%s' is: %d\n", test, len);
+	return (0);
+}*/
