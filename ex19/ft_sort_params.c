370f042151319e3f8e9d2e7cdfe7271884969f0a
emmmilla <emmmilla@student.42madrid.com>
2026-05-25 23:50:18 +0200

    First Upload ex19

---
+++ ex19/ft_sort_params.c
@@ -0,0 +1,81 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sqrt.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: emmmilla <emmmilla@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/22 13:19:00 by emmmilla          #+#    #+#             */
+/*   Updated: 2025/11/20 19:11:13 by emmmilla         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putstr(char *s)
+{
+	int	i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		ft_putchar(s[i]);
+		i++;
+	}
+}
+
+int	ft_strcmp(char *s1, char *s2)
+{
+	int	i;
+
+	i = 0;
+	while (s1[i] == s2[i] && s1[i] && s2[i])
+		i++;
+	return (s1[i] - s2[i]);
+}
+
+void	ft_sort_params(char **av, int ac)
+{
+	int		i;
+	int		j;
+	char	*tmp;
+
+	i = 1;
+	while (i < ac - 1)
+	{
+		j = 1;
+		while (j < ac - i)
+		{
+			if (ft_strcmp(av[j], av[j + 1]) > 0)
+			{
+				tmp = av[j];
+				av[j] = av[j + 1];
+				av[j + 1] = tmp;
+			}
+			j++;
+		}
+		i++;
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	int	i;
+
+	if (argc < 2)
+		return (0);
+	ft_sort_params(argv, argc);
+	i = 1;
+	while (i < argc)
+	{
+		ft_putstr(argv[i]);
+		ft_putchar('\n');
+		i++;
+	}
+	return (0);
+}
