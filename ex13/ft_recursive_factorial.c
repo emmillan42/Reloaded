77ee1926a6ae15874eff77d7c8abb6f91a3d0faa
emmmilla <emmmilla@student.42madrid.com>
2026-05-27 17:32:40 +0200

    Update ex13

--- ex13/ft_recursive_factorial.c
+++ ex13/ft_recursive_factorial.c
@@ -6,14 +6,14 @@
 /*   By: emmmilla <emmmilla@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/22 12:19:53 by emmmilla          #+#    #+#             */
-/*   Updated: 2026/05/26 13:53:19 by emmmilla         ###   ########.fr       */
+/*   Updated: 2026/05/27 17:32:14 by emmmilla         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 //Recursive function that returns the factorial of the integer parameter number
 int	ft_recursive_factorial(int nb)
 {
-	if (nb < 0)
+	if (nb < 0 || nb > 12)
 		return (0);
 	if (nb == 0)
 		return (1);
@@ -29,7 +29,7 @@ int	main(void)
 	int	n;
 
 	n = -2;
-	while (n <= 13)
+	while (n <= 15)
 	{
 		printf("The %d! is: %d\n", n, ft_recursive_factorial(n));
 		n++;
