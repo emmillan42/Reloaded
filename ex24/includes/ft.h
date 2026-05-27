e81847c5321bc6f8a9c2373252bf90a9f273cb7a
emmmilla <emmmilla@student.42madrid.com>
2026-05-27 12:27:20 +0200

    First Upload ex24

---
+++ ex24/includes/ft.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_h.h                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: emmmilla <emmmilla@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/27 10:06:38 by emmmilla          #+#    #+#             */
+/*   Updated: 2026/05/27 10:10:17 by emmmilla         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_H
+# define FT_H
+
+void	ft_putchar(char c);
+void	ft_putstr(char *s);
+void	ft_swap(int *a, int *b);
+int		ft_strlen(char *s);
+int		ft_strcmp(char *s1, char *s2);
+
+#endif
