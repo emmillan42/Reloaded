919d7753bc4c609a9c8e32656bd4dbb17b02fdd8
emmmilla <emmmilla@student.42madrid.com>
2026-05-27 20:17:26 +0200

    Update ex27

--- ex27/ft_display_file.c
+++ ex27/ft_display_file.c
@@ -71,12 +71,12 @@ int	main(int argc, char **argv)
 	if (argc < 2)
 	{
 		write(2, "File name missing.\n", 19);
-		return (1);
+		return (0);
 	}
 	if (argc > 2)
 	{
-		write(2, "Too many arguments.\n", 21);
-		return (1);
+		write(2, "Too many arguments.\n", 20);
+		return (0);
 	}
 	if (!ft_show_file(argv[1]))
 	{
