1c0d5d93337e9138ffa5f3ef4e446cd725556df7
emmmilla <emmmilla@student.42madrid.com>
2026-05-27 13:36:52 +0200

    First Upload ex25

---
+++ ex25/ft_foreach.c
@@ -0,0 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_foreach.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: emmmilla <emmmilla@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/27 13:02:00 by emmmilla          #+#    #+#             */
+/*   Updated: 2026/05/27 13:35:48 by emmmilla         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putnbr(int nb)
+{
+	long	n;
+
+	n = nb;
+	if (n < 0)
+	{
+		ft_putchar('-');
+		n = -n;
+	}
+	if (n > 9)
+	{
+		ft_putnbr(n / 10);
+		n %= 10;
+	}
+	ft_putchar(n + '0');
+}
+*/
+void	ft_foreach(int *tab, int length, void (*f)(int))
+{
+	int	i;
+
+	i = 0;
+	while (i < length)
+	{
+		f(tab[i]);
+		i++;
+	}
+}
+
+/*//Testing the function with some values
+
+int	main(void)
+{
+	int	tab[10] = {42, 76, -3, 92, -2147483648, 2147483647, 0, 10, -9, -10000};
+
+	ft_foreach(tab, 10, &ft_putnbr);
+	return (0);
+}*/
